2a2204459ae6e9a3b0eb3974bc6e2e0eb597d432
Adam Kurzyna <98416378+jkr75@users.noreply.github.com>
2023-05-23 14:07:22 +0200

    Add files via upload

--- src/app/welcome-page-component/welcome-page-component.component.ts
+++ src/app/welcome-page-component/welcome-page-component.component.ts
@@ -2,6 +2,7 @@ import { Component, Output, EventEmitter, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { StorageService } from '../storage.service';
 import { ActivatedRoute } from '@angular/router';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 
 interface FormData {
   Name: string;
@@ -13,79 +14,86 @@ interface FormData {
   templateUrl: './welcome-page-component.component.html',
   styleUrls: ['./welcome-page-component.component.css'],
 })
-
 export class WelcomePageComponentComponent implements OnInit {
+  playerForm: FormGroup;
+  selectedColors: string = 'normal_colors';
 
   @Output() public WelcomeToGameEvent = new EventEmitter<boolean>();
   @Output() public playerInfoEvent = new EventEmitter<Player>();
-  @Output() public palyerDataEvent = new EventEmitter<Array<Player>>();
+  @Output() public playerDataEvent = new EventEmitter<Array<Player>>();
 
   public ErrorAlert: boolean = false;
 
-  constructor(private _router: Router, private _storage: StorageService, private _route: ActivatedRoute) {
+  constructor(
+    private _router: Router,
+    private _storage: StorageService,
+    private _route: ActivatedRoute,
+    private _fb: FormBuilder
+  ) {
     this._router.navigate(['/welcome']);
-
+    this.playerForm = this._fb.group({
+      Name: ['', [Validators.required, Validators.minLength(3)]],
+      Email: ['', [Validators.required, Validators.email]],
+      selectedColors: ['normal_colors'],
+    });
   }
-
-  public selectedColors: string = '';
 
   public goToColors() {
     this.sendPlayerInfo();
-    console.log(this.selectedColors, 'welcome: ' + this.selectedColors)
+    console.log(this.selectedColors, 'welcome: ' + this.selectedColors);
     if (this.selectedColors === 'normal_colors') {
-      this._router.navigate(['/gry', 'high_contrast'], {
-        relativeTo: this._route
-      })
+      this._router.navigate(['/gry', 'normal_colors'], {
+        relativeTo: this._route,
+      });
     } else {
-      this._router.navigate(['/gry', 'normal_colors'])
-    };
+      this._router.navigate(['/gry', 'high_contrast']);
+    }
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   public changeErrorAlert() {
     this.ErrorAlert = false;
   }
 
   public onSubmitForm() {
-    this.goToColors();
-    this.sendStatus();
-    this.sendFormInfo();
+    if (this.playerForm.valid) {
+      const formValue = this.playerForm.value;
+      console.log(formValue);
+      this.goToColors();
+      this.sendStatus();
+      this.sendFormInfo();
+      this._storage.setUserData(formValue.Name);
+    }
   }
-
-  public onSubmit(formValue: FormData) {
-    this._storage.setUserData(formValue.Name);
-    // this._router.navigate(['/gry'])
-  }
-
+  
   public playerInfo: Player = {
     Name: '',
-    Email: ''
-  }
+    Email: '',
+  };
 
-  public playerData: Array<Player> = []
+  public playerData: Array<Player> = [];
   public changeComps: boolean = true;
 
   sendStatus() {
-    this.changeComps = !this.changeComps,
-      this.WelcomeToGameEvent.emit(this.changeComps)
+    this.changeComps = !this.changeComps;
+    this.WelcomeToGameEvent.emit(this.changeComps);
   }
 
   sendPlayerInfo() {
     this.playerInfoEvent.emit({
       Name: this.playerInfo.Name,
       Email: this.playerInfo.Email,
-    })
-
+    });
   }
+
   sendFormInfo() {
-    this._storage.setUserData(this.playerInfo.Name)
+    this._storage.setUserData(this.playerInfo.Name);
   }
 }
 
 export interface Player {
-  Name: string,
-  Email: string,
-  pAction?: object,
+  Name: string;
+  Email: string;
+  pAction?: object;
 }
