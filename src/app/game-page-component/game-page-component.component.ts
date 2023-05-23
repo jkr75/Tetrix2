4ef91b7cd4b3c3cdf43b896dce81403de524adf5
Adam Kurzyna <98416378+jkr75@users.noreply.github.com>
2023-05-23 20:11:32 +0200

    Add files via upload

--- src/app/game-page-component/game-page-component.component.ts
+++ src/app/game-page-component/game-page-component.component.ts
@@ -1,82 +1,87 @@
-import { Component, EventEmitter, Input, Output, ViewChild, OnInit, Injectable } from '@angular/core';
+import { Component, EventEmitter, Input, Output, ViewChild, OnInit } from '@angular/core';
 import { Player } from '../welcome-page-component/welcome-page-component.component';
 import { TetrisCoreComponent } from 'ngx-tetris';
 import { StorageService } from '../storage.service';
 import { Router } from '@angular/router';
 import { ActivatedRoute } from '@angular/router';
 import { ColorService } from '../color.service';
+import { PlayerDataService } from '../player-data.service';
 
 @Component({
   selector: 'app-game-page-component',
   templateUrl: './game-page-component.component.html',
   styleUrls: ['./game-page-component.component.css'],
 })
-
-@Injectable()
 export class GamePageComponentComponent implements OnInit {
+  public selectedColors: string = '';
+  playerData: any;
 
   @Input() playerInfo: Player;
-  @Input() playerData: Array<Player>;
   @Output() public GameToWelcomeEvent = new EventEmitter<boolean>();
 
   @ViewChild(TetrisCoreComponent)
+  private tetris!: TetrisCoreComponent;
 
-  private tetrix!: TetrisCoreComponent;
+  public showS: boolean = true;
   public swapToTitle: boolean = true;
-  public pointCount: number = 0
+  public pointCount: number = 0;
   public isReady: boolean = true;
   public isGo: boolean = false;
   public isPaused: boolean = false;
   public isGameOver: boolean = false;
   public gameplayTimer: number = 0;
   public Interval: any;
-  public gamePlay: boolean = true;
-  public showS: boolean = true;
   public gameHistory: Array<pAction> = [];
   public saveCurrentGameData: TotalGameData = {
     playerName: '',
     pointC: 0,
     timeP: 0,
     gameHistory: [],
-  }
+  };
 
   public onStartButton() {
-    this.tetrix.actionStart();
-    this.PushCurrentData("actionStart");
+    this.tetris.actionStart();
+    this.PushCurrentData('actionStart');
     this.GameStarted();
   }
+
   public onStopButton() {
-    this.tetrix.actionStop();
-    this.PushCurrentData("actionStop");
+    this.tetris.actionStop();
+    this.PushCurrentData('actionStop');
     this.GamePaused();
   }
+
   public onResetButton() {
-    this.tetrix.actionReset();
-    this.PushCurrentData("actionReset");
+    this.tetris.actionReset();
+    this.PushCurrentData('actionReset');
     this.GameReady();
   }
+
   public onLeftButton() {
-    this.tetrix.actionLeft();
-    this.PushCurrentData("actionLeft");
+    this.tetris.actionLeft();
+    this.PushCurrentData('actionLeft');
   }
+
   public onRightButton() {
-    this.tetrix.actionRight();
-    this.PushCurrentData("actionRight");
+    this.tetris.actionRight();
+    this.PushCurrentData('actionRight');
   }
+
   public onRotateButton() {
-    this.tetrix.actionRotate();
-    this.PushCurrentData("actionRotate")
+    this.tetris.actionRotate();
+    this.PushCurrentData('actionRotate');
   }
+
   public onDownButton() {
-    this.tetrix.actionDown();
-    this.PushCurrentData('actionDown')
+    this.tetris.actionDown();
+    this.PushCurrentData('actionDown');
   }
 
   public PushCurrentData(message: string) {
     this.gameHistory.push({
       Time: this.gameplayTimer,
-      Action: message
-    })
+      Action: message,
+    });
   }
 
   public saveCurrentGamePlay() {
@@ -84,26 +89,28 @@ export class GamePageComponentComponent implements OnInit {
       playerName: this.playerInfo.Name,
       pointC: this.pointCount,
       timeP: this.gameplayTimer,
-      gameHistory: this.gameHistory
-    }
-    this.gameHistory = []
+      gameHistory: this.gameHistory,
+    };
+    this.gameHistory = [];
   }
 
   sendStatus() {
     this.GameToWelcomeEvent.emit(this.swapToTitle);
     this.stopTimer();
   }
+
   onFoodEaten() {
     this.pointCount = this.pointCount + 1;
-    this.PushCurrentData("Food Eaten")
+    this.PushCurrentData('Food Eaten');
   }
+
   onGameOver() {
     this.isGameOver = true;
     this.isPaused = false;
     this.isGo = false;
     this.isReady = false;
     this.stopTimer();
-    this.PushCurrentData("Game Over");
+    this.PushCurrentData('Game Over');
   }
 
   GamePaused() {
@@ -135,7 +142,7 @@ export class GamePageComponentComponent implements OnInit {
   startTimer() {
     this.Interval = setInterval(() => {
       this.gameplayTimer++;
-    }, 1000)
+    }, 1000);
   }
 
   stopTimer() {
@@ -143,41 +150,39 @@ export class GamePageComponentComponent implements OnInit {
   }
 
   public statS() {
-    this.showS = !this.showS;
     this.saveCurrentGamePlay();
   }
 
-  public selectedColors: string = '';
-
   constructor(
-    private router: Router, 
-    private _storage: StorageService, 
-    private _route: ActivatedRoute, 
-    private colorService: ColorService
-    ) {
-    this.selectedColors = colorService.selectedColors;
+    private router: Router,
+    private _storage: StorageService,
+    private _route: ActivatedRoute,
+    private colorService: ColorService,
+    private _playerDataService: PlayerDataService
+  ) {
     this.playerInfo = {
       Name: this._storage.readPlayerName(),
       Email: '',
-    }
+    };
 
     this.playerData = [];
   }
- 
+
   goToColors(selectedColors: string) {
-    this.colorService.selectedColors = this.selectedColors;
-    console.log(this.selectedColors, 'gry: ' + this.selectedColors)
-    if (selectedColors === 'normal_colors') {
+    console.log(this.selectedColors, 'gry: ' + this.selectedColors);
+    if (this.selectedColors === 'normal_colors') {
       this.router.navigate(['/gry', 'high_contrast'], {
-        relativeTo: this._route
-      })
+        relativeTo: this._route,
+      });
     } else {
-      this.router.navigate(['/gry', 'normal_colors'])
-    };
+      this.router.navigate(['/gry', 'normal_colors']);
+    }
   }
 
   ngOnInit() {
-    this._route.params.subscribe(params => {
+    const playerData = this._playerDataService.getPlayerData();
+    this.selectedColors = playerData?.selectedColors || 'normal_colors';
+    this._route.params.subscribe((params) => {
       if (params.hasOwnProperty('colors')) {
         this.selectedColors = params['colors'];
       } else {
@@ -192,6 +197,7 @@ export interface pAction {
   Time: number;
   Action: string;
 }
+
 export interface TotalGameData {
   playerName: string;
   gameHistory: Array<pAction>;
