4728f079153e13b59b556732ddd9761933064d19
Adam Kurzyna <98416378+jkr75@users.noreply.github.com>
2023-06-18 16:38:43 +0200

    Add files via upload

--- src/app/welcome-page-component/welcome-page-component.component.ts
+++ src/app/welcome-page-component/welcome-page-component.component.ts
@@ -4,12 +4,13 @@ import { StorageService } from '../storage.service';
 import { ActivatedRoute } from '@angular/router';
 import { FormGroup, Validators, FormBuilder, FormControl } from '@angular/forms';
 import { PlayerDataService } from '../player-data.service';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { ColorService } from '../color.service';
 
 interface FormData {
   Name: string;
-  Email: string;
+  // Email: string;
+  Token: string;
 }
 
 @Component({
@@ -17,6 +18,7 @@ interface FormData {
   templateUrl: './welcome-page-component.component.html',
   styleUrls: ['./welcome-page-component.component.css'],
 })
+
 export class WelcomePageComponentComponent implements OnInit {
   playerForm: FormGroup;
   public selectedColors: string = '';
@@ -34,19 +36,18 @@ export class WelcomePageComponentComponent implements OnInit {
     private _fb: FormBuilder,
     private _playerDataService: PlayerDataService,
     private _http: HttpClient,
-    private colorService: ColorService,
+    private _colorService: ColorService,
   ) {
     this._router.navigate(['/welcome']);
-    this.playerForm = this._fb.group({
-      Name: ['', [Validators.required, Validators.minLength(5)]],
-      // Token: ['', [Validators.required, Validators.minLength(4)]],
-      Email: ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$')]],
-      selectedColors: ['normal_colors'],
+    this.playerForm = new FormGroup({
+      Name: new FormControl('', [Validators.required, Validators.minLength(5)]),
+      Token: new FormControl('', [Validators.required, Validators.pattern('^[0-9]{4,}$')]),
+      selectedColors: new FormControl('normal_colors'),
     });
   }
 
   public goToColors(selectedColors: string) {
-    this.colorService.selectedColors = selectedColors;
+    this._colorService.selectedColors = selectedColors;
     console.log(selectedColors, 'welcome: ' + selectedColors);
     if (selectedColors === 'normal_colors') {
       this._router.navigate(['/gry', 'normal_colors'], {
@@ -64,9 +65,15 @@ export class WelcomePageComponentComponent implements OnInit {
       });
     }
   }
+  
 
   ngOnInit() {
-    this.selectedColors = this.colorService.selectedColors;
+    this.selectedColors = this._colorService.selectedColors;
+    this.playerForm = this._fb.group({
+      Name: ['', [Validators.required, Validators.minLength(5)]],
+      Token: ['', [Validators.required, Validators.pattern('^[0-9]{4,}$')]],
+      selectedColors: ['normal_colors'],
+    });
   }
 
   public changeErrorAlert() {
@@ -77,29 +84,36 @@ export class WelcomePageComponentComponent implements OnInit {
     if (this.playerForm.valid) {
       const formValue = this.playerForm.value;
       const selectedColors = this.playerForm.value.selectedColors;
-      this._router.navigate(['/game', selectedColors]);
+      this._router.navigate(['/gry', selectedColors]);
       console.log(formValue);
-      this.goToColors(selectedColors);
+      // this.goToColors()
       this.sendStatus();
       this.sendFormInfo();
       this._storage.setUserData(formValue.Name);
       this._playerDataService.setPlayerData(formValue);
-      // if (formValue.Token) {
-      //   this._http.post('http://localhost:4200/check-token', { Token: formValue.Token }).subscribe(
-      //     (response) => {
-      //       console.log('Token jest poprawny');
-      //     },
-      //     (error) => {
-      //       console.error('Nieprawidłowy token:', error);
-      //     }
-      //   );
-      // }
+      if (formValue.Token) {
+        const httpOptions = {
+          headers: new HttpHeaders({
+            'Content-Type': 'application/json'
+          })
+        };
+        console.log('string token:', formValue.Token)
+        this._http.post('http://scores.chrum.it/check-token', { Token: formValue.Token }, httpOptions).subscribe(
+          (response) => {
+            console.log('Token jest poprawny');
+          },
+          (error) => {
+            console.error('Nieprawidłowy token:', error);
+          }
+        );
+      }
     }
   }
 
+
   public playerInfo: Player = {
     Name: '',
-    Email: '',
+    // Emai: '',
   };
 
   public playerData: Array<Player> = [];
@@ -113,7 +127,6 @@ export class WelcomePageComponentComponent implements OnInit {
   sendPlayerInfo() {
     this.playerInfoEvent.emit({
       Name: this.playerInfo.Name,
-      Email: this.playerInfo.Email,
     });
   }
 
@@ -124,5 +137,5 @@ export class WelcomePageComponentComponent implements OnInit {
 
 export interface Player {
   Name: string;
-  Email: string;
+  pAction?: object;
 }
